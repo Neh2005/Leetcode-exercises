0d416162053ee5a76d443e41cab402be91e53ca1
Neha Ann Binoy <168305581+Neh2005@users.noreply.github.com>
2024-11-05 13:48:42 +0000

    Update Add_sum_hashmap.py

--- Array-easy/Add_sum_hashmap.py
+++ Array-easy/Add_sum_hashmap.py
@@ -9,3 +9,6 @@ class Solution:
             if sum in hashmap:
                 return [hashmap[sum],i]
             hashmap[nums[i]]=i
+
+"""Hashmap is the most efficient method here to avoid duplicates and give the two numbers that add up to return the target from the given list."""
+"""The time complexity is O(n) and space complexity of O(1)"""
